dbe8543d84e9f32d7f0520fcdc23b04184f35908
An222n <tumie16@gmail.com>
2022-09-22 13:06:14 +0200

    5. Using printf function to display stdout

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
- * Description: main - Prints out a string to stdout
+ * main- Entry point
+ * Description: Prints out a string to stdout
+ *
  * Return: always 0 (success)
  */
 int main(void)
-{	
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+{
+	printf("with proper grammar, but the outcome is a piece of art,/n");
 	return (0);
 }
