36845a5507308bf14b9b7eef34f89f381aa80f88
An222n <tumie16@gmail.com>
2022-09-23 00:06:55 +0200

    101. Using fputs function to display output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
  * main- Entry point
- *
- * Return: 0 if successful
+ * Return: 1 if successful
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (0);
+	fputs("and that piece of art is useful
+	\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
